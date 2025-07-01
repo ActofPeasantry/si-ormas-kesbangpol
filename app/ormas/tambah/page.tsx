874fa1d16fc3afc893d26352b9d3d36c6b2d89e0
Afif Maulana Isman <43715259+ActofPeasantry@users.noreply.github.com>
2025-07-01 10:27:41 +0700

    feat: make ormas/tambah form function (still WIP) -add addOrmasData query -add handleSubmit() on tambah/ormas page

--- app/ormas/tambah/page.tsx
+++ app/ormas/tambah/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
@@ -12,22 +14,76 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { addOrmasData } from "@/lib/queries/ormas";
 
-export default function addOrmas() {
-  const breadcrumb = [
-    {
-      title: "Dashboard",
-      url: "/",
-    },
-    {
-      title: "Ormas",
-      url: "/ormas",
-    },
-    {
-      title: "Tambah Ormas",
-      url: "/ormas/add",
-    },
-  ];
+const breadcrumb = [
+  {
+    title: "Dashboard",
+    url: "/",
+  },
+  {
+    title: "Ormas",
+    url: "/ormas",
+  },
+  {
+    title: "Tambah Ormas",
+    url: "/ormas/add",
+  },
+];
+
+export default function Page() {
+  const router = useRouter();
+
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [namaOrmas, setNamaOrmas] = useState<string>("");
+  const [singkatanOrmas, setSingkatanOrmas] = useState<string>("");
+  const [alamatOrmas, setAlamatOrmas] = useState<string>("");
+  const [noTelpOrmas, setNoTelpOrmas] = useState<string>("");
+  const [skBadanHukum, setSkBadanHukum] = useState<string>("");
+  const [skBadanKeperguruan, setSkBadanKeperguruan] = useState<string>("");
+  const [adArt, setAdArt] = useState<string>("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData();
+    const fields = {
+      //user data
+      username: username,
+      email: email,
+      password: "12345678",
+      //ormas data
+      namaOrmas: namaOrmas,
+      singkatanOrmas: singkatanOrmas,
+      alamatOrmas: alamatOrmas,
+      noTelpOrmas: noTelpOrmas,
+      skBadanHukum: skBadanHukum,
+      skBadanKeperguruan: skBadanKeperguruan,
+      adArt: adArt,
+    };
+
+    Object.entries(fields).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    await addOrmasData(formData);
+
+    try {
+      setUsername("");
+      setEmail("");
+      setNamaOrmas("");
+      setSingkatanOrmas("");
+      setAlamatOrmas("");
+      setNoTelpOrmas("");
+      setSkBadanHukum("");
+      setSkBadanKeperguruan("");
+      setAdArt("");
+      console.log("submit");
+      router.push("/ormas"); // 🔀 redirect
+    } catch (error) {
+      console.error("Error inserting data:", error);
+    }
+  };
 
   return (
     <SidebarProvider
@@ -46,126 +102,170 @@ export default function addOrmas() {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <div className="px-4 lg:px-6">
-                <Card className="@container/card">
-                  <CardHeader>
-                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-                      Registrasi Organisasi Masyarakat
-                    </CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="mb-4">
-                      <h1>INFORMASI AKUN USER ORGANISASI</h1>
-                      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3 lg:mb-0">
-                        <div>
-                          <Label className="mb-2" htmlFor="username">
-                            Username<span className="text-red-500">*</span>
-                          </Label>
-                          <Input
-                            id="username"
-                            type="text"
-                            placeholder="Username"
-                          />
-                        </div>
-                        <div>
-                          <Label className="mb-2" htmlFor="email">
-                            Email<span className="text-red-500">*</span>
-                          </Label>
-                          <Input id="email" type="email" placeholder="Email" />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="mb-4">
-                      <h1>INFORMASI ORGANISASI MASYARAKAT</h1>
-                      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3 lg:mb-0">
-                        <div>
-                          <Label className="mb-2" htmlFor="namaOrmas">
-                            Nama Ormas<span className="text-red-500">*</span>
-                          </Label>
-                          <Input
-                            id="namaOrmas"
-                            type="text"
-                            placeholder="Nama Ormas"
-                          />
-                        </div>
-                        <div>
-                          <Label className="mb-2" htmlFor="singkatanOrmas">
-                            Singkatan Ormas
-                            <span className="text-red-500">*</span>
-                          </Label>
-                          <Input
-                            id="singkatanOrmas"
-                            type="text"
-                            placeholder="Singkatan Ormas"
-                          />
+                <form onSubmit={handleSubmit}>
+                  <Card className="@container/card">
+                    <CardHeader>
+                      <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                        Registrasi Organisasi Masyarakat
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="mb-4">
+                        <h1>INFORMASI AKUN USER ORGANISASI</h1>
+                        <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3 lg:mb-0">
+                          <div>
+                            <Label className="mb-2" htmlFor="username">
+                              Username<span className="text-red-500">*</span>
+                            </Label>
+                            <Input
+                              id="username"
+                              type="text"
+                              placeholder="Username"
+                              value={username}
+                              onChange={(e) => setUsername(e.target.value)}
+                              required
+                            />
+                          </div>
+                          <div>
+                            <Label className="mb-2" htmlFor="email">
+                              Email<span className="text-red-500">*</span>
+                            </Label>
+                            <Input
+                              id="email"
+                              type="email"
+                              placeholder="Email"
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}
+                              required
+                            />
+                          </div>
                         </div>
                       </div>
-                      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3 lg:mb-0">
-                        <div>
-                          <Label className="mb-2" htmlFor="alamatOrmas">
-                            Alamat Ormas
-                            <span className="text-red-500">*</span>
-                          </Label>
-                          <Input
-                            id="alamatOrmas"
-                            type="text"
-                            placeholder="Alamat Ormas"
-                          />
+                      <div className="mb-4">
+                        <h1>INFORMASI ORGANISASI MASYARAKAT</h1>
+                        <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3 lg:mb-0">
+                          <div>
+                            <Label className="mb-2" htmlFor="namaOrmas">
+                              Nama Ormas<span className="text-red-500">*</span>
+                            </Label>
+                            <Input
+                              id="namaOrmas"
+                              type="text"
+                              placeholder="Nama Ormas"
+                              value={namaOrmas}
+                              onChange={(e) => setNamaOrmas(e.target.value)}
+                              required
+                            />
+                          </div>
+                          <div>
+                            <Label className="mb-2" htmlFor="singkatanOrmas">
+                              Singkatan Ormas
+                              <span className="text-red-500">*</span>
+                            </Label>
+                            <Input
+                              id="singkatanOrmas"
+                              type="text"
+                              placeholder="Singkatan Ormas"
+                              value={singkatanOrmas}
+                              onChange={(e) =>
+                                setSingkatanOrmas(e.target.value)
+                              }
+                              required
+                            />
+                          </div>
                         </div>
-                        <div>
-                          <Label className="mb-2" htmlFor="noTelpOrmas">
-                            Nomor Telepon Ormas
-                            <span className="text-red-500">*</span>
-                          </Label>
-                          <Input
-                            id="noTelpOrmas"
-                            type="text"
-                            placeholder="Nomor Telepon Ormas"
-                          />
+                        <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3 lg:mb-0">
+                          <div>
+                            <Label className="mb-2" htmlFor="alamatOrmas">
+                              Alamat Ormas
+                              <span className="text-red-500">*</span>
+                            </Label>
+                            <Input
+                              id="alamatOrmas"
+                              type="text"
+                              placeholder="Alamat Ormas"
+                              value={alamatOrmas}
+                              onChange={(e) => setAlamatOrmas(e.target.value)}
+                              required
+                            />
+                          </div>
+                          <div>
+                            <Label className="mb-2" htmlFor="noTelpOrmas">
+                              Nomor Telepon Ormas
+                              <span className="text-red-500">*</span>
+                            </Label>
+                            <Input
+                              id="noTelpOrmas"
+                              type="text"
+                              placeholder="Nomor Telepon Ormas"
+                              value={noTelpOrmas}
+                              onChange={(e) => setNoTelpOrmas(e.target.value)}
+                              required
+                            />
+                          </div>
+                        </div>
+                        <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3 lg:mb-0">
+                          <div>
+                            <Label className="mb-2" htmlFor="skBadanHukum">
+                              SK Badan Hukum
+                              <span className="text-red-500">*</span>
+                            </Label>
+                            <Input
+                              id="skBadanHukum"
+                              type="text"
+                              placeholder="SK Badan Hukum"
+                              value={skBadanHukum}
+                              onChange={(e) => setSkBadanHukum(e.target.value)}
+                              required
+                            />
+                          </div>
+                          <div>
+                            <Label
+                              className="mb-2"
+                              htmlFor="skBadanKeperguruan"
+                            >
+                              SK Badan Keperguruan
+                              <span className="text-red-500">*</span>
+                            </Label>
+                            <Input
+                              id="skBadanKeperguruan"
+                              type="text"
+                              placeholder="SK Badan Keperguruan"
+                              value={skBadanKeperguruan}
+                              onChange={(e) =>
+                                setSkBadanKeperguruan(e.target.value)
+                              }
+                              required
+                            />
+                          </div>
+                          <div>
+                            <Label className="mb-2" htmlFor="adArtadArt">
+                              AD/ART
+                              <span className="text-red-500">*</span>
+                            </Label>
+                            <Input
+                              id="adArt"
+                              type="text"
+                              placeholder="AD/ART"
+                              value={adArt}
+                              onChange={(e) => setAdArt(e.target.value)}
+                              required
+                            />
+                          </div>
+                        </div>
+                        <div className="p-6 gap-3 mb-3 ">
+                          <Button variant="outline">Submit</Button>
                         </div>
                       </div>
-                      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-3 lg:mb-0">
-                        <div>
-                          <Label className="mb-2" htmlFor="skBadanHukum">
-                            SK Badan Hukum
-                            <span className="text-red-500">*</span>
-                          </Label>
-                          <Input
-                            id="skBadanHukum"
-                            type="text"
-                            placeholder="SK Badan Hukum"
-                          />
-                        </div>
-                        <div>
-                          <Label className="mb-2" htmlFor="skBadanKeperguruan">
-                            SK Badan Keperguruan
-                            <span className="text-red-500">*</span>
-                          </Label>
-                          <Input
-                            id="skBadanKeperguruan"
-                            type="text"
-                            placeholder="SK Badan Keperguruan"
-                          />
-                        </div>
-                        <div>
-                          <Label className="mb-2" htmlFor="adArtadArt">
-                            AD/ART
-                            <span className="text-red-500">*</span>
-                          </Label>
-                          <Input id="adArt" type="text" placeholder="AD/ART" />
-                        </div>
-                      </div>
-                      <div className="p-6 gap-3 mb-3 ">
-                        <Button variant="outline">Submit</Button>
-                      </div>
-                    </div>
-                  </CardContent>
+                    </CardContent>
 
-                  <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                    <p>
-                      <span className="text-red-500">*</span> : Wajib Diisi
-                    </p>
-                  </CardFooter>
-                </Card>
+                    <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                      <p>
+                        <span className="text-red-500">*</span> : Wajib Diisi
+                      </p>
+                    </CardFooter>
+                  </Card>
+                </form>
               </div>
             </div>
           </div>
---
+++ lib/queries/ormas.ts
@@ -0,0 +1,74 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { DetailOrmasTable, OrmasTable, UsersTable } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+
+export async function getOrmasData() {
+  const result = await db
+    .select({
+      namaOrmas: OrmasTable.namaOrmas,
+      singkatanOrmas: OrmasTable.singkatanOrmas,
+      noTelpOrmas: DetailOrmasTable.noTelpOrmas,
+      alamatOrmas: DetailOrmasTable.alamatOrmas,
+    })
+    .from(OrmasTable)
+    .leftJoin(DetailOrmasTable, eq(OrmasTable.id, DetailOrmasTable.OrmasId));
+  return result;
+}
+
+export async function addOrmasData(formData: FormData) {
+  // Users data
+  const username = formData.get("username") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  // Ormas data
+  const namaOrmas = formData.get("namaOrmas") as string;
+  const singkatanOrmas = formData.get("singkatanOrmas") as string;
+
+  // Detail Ormas
+  const alamatOrmas = formData.get("alamatOrmas") as string;
+  const noTelpOrmas = formData.get("noTelpOrmas") as string;
+  const skBadanHukum = formData.get("skBadanHukum") as string;
+  const skBadanKeperguruan = formData.get("skBadanKeperguruan") as string;
+  const adArt = formData.get("adArt") as string;
+
+  try {
+    // Insert user
+    const userResult = await db
+      .insert(UsersTable)
+      .values({ username, email, password, role: "akun ormas" })
+      .$returningId();
+
+    const userId = userResult[0].id;
+
+    // Insert ormas
+    const ormasResult = await db
+      .insert(OrmasTable)
+      .values({
+        userId: userId,
+        namaOrmas,
+        singkatanOrmas,
+        statusOrmas: "Non Aktif",
+      })
+      .$returningId();
+
+    const ormasId = ormasResult[0].id;
+
+    // Insert detail_ormas
+    await db.insert(DetailOrmasTable).values({
+      OrmasId: ormasId,
+      alamatOrmas,
+      noTelpOrmas,
+      skBadanHukum,
+      skBadanKeperguruan,
+      adArt,
+    });
+
+    console.log("Inserted userId:", userId);
+    console.log("Inserted ormasId:", ormasId);
+  } catch (error) {
+    console.error("Error inserting data:", error);
+  }
+}
